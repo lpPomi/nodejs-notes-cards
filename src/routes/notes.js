2e906a8c830c5dd46490d06d00c14b78845f0224
lpPomi <lp.chamba@gmail.com>
2019-05-04 19:48:03 +0200

    CRUD part1

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -10,17 +10,39 @@
   layoutsDir: path.join(app.get('views'), 'layouts'),
   */
 
-// method Router to create the routes
-// so
-//const router = require('express').Router();
-//or so
+/* method Router to create the routes
+   so: const router = require('express').Router();
+   or so: 
+*/
+
 const express = require ('express');
 const router = express.Router();
 
-// define the routes
+// define the routes to add a note
+router.get('/notes/add', (req, res) => {
+  res.render('notes/new-note');
+});
+
+
+/* define the routes to get data e.g. from a form
+   and the send this form data to: 
+   /notes/new-note 
+*/
+router.post('/notes/new-note' , (req, res) => {
+  console.log(req.body);
+  res.send('ok');
+});
+
+
+
+// test define the route notes
+/*
 router.get('/notes', (req, res) => {
     res.send('Notas de la database');
   });
+
+*/
+
 
 // export the router
 module.exports = router;
